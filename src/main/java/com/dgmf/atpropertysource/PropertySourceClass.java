3ac2049465cc0e12d81a6ebaf28416ef71fd3a2a
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-14 05:01:58 +0100

    Using @PropertySource and Native Environment class to read Property files - Step 3

--- src/main/java/com/dgmf/atpropertysource/PropertySourceClass.java
+++ src/main/java/com/dgmf/atpropertysource/PropertySourceClass.java
@@ -1,11 +1,13 @@
 package com.dgmf.atpropertysource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropertySourceClass {
-    @Value("${gmail.host}")
+    /*@Value("${gmail.host}")
     private String host;
     @Value("${gmail.email}")
     private String email;
@@ -14,25 +16,27 @@ public class PropertySourceClass {
     @Value("${app.name}")
     private String name;
     @Value("${app.description}")
-    private String description;
+    private String description;*/
+    @Autowired
+    private Environment environment;
 
     public String getHost() {
-        return host;
+        return environment.getProperty("hotmail.host");
     }
 
     public String getEmail() {
-        return email;
+        return environment.getProperty("hotmail.email");
     }
 
     public String getPassword() {
-        return password;
+        return environment.getProperty("hotmail.password");
     }
 
     public String getName() {
-        return name;
+        return environment.getProperty("app.name");
     }
 
     public String getDescription() {
-        return description;
+        return environment.getProperty("app.description");
     }
 }
