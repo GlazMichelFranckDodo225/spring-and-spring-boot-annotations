88e92887b21f9d733e7ee8126b6e6241bf8559f4
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-22 11:05:10 +0100

    @Controller, @RequestMapping and @ResponseBody Annotations - V2

--- src/main/java/com/dgmf/atcontrollerannotation/controller/BookController.java
+++ src/main/java/com/dgmf/atcontrollerannotation/controller/BookController.java
@@ -1,15 +1,36 @@
 package com.dgmf.atcontrollerannotation.controller;
 
+import com.dgmf.atcontrollerannotation.entity.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+// To make this Spring MVC Controller to
+// handle incoming HTTP Requests
+@Controller // Specialization of "@Component" Annotation
 public class BookController {
-    @RequestMapping("/hello")
-    @ResponseBody // To return Json Response to the Client
+    // Handler Method
+    @RequestMapping("/hello") // Map the incoming HTTP Request
+    // to this Method
+    @ResponseBody // To convert into Json format the returned
+    // Response to the Client
     public String hello() {
         return "Hello world !";
+    }
+
+    // Handler Method
+    @RequestMapping("/book") // Map the incoming HTTP Request
+    // to this Method
+    @ResponseBody // To convert into Json format the returned
+    // Response to the Client
+    public Book getBook() {
+        Book book = new Book(
+                1L,
+                "Core Spring Framework",
+                "Spring Annotations"
+        );
+
+        return book;
     }
 }
